177f556bb5559dcce59943b82dd18beb3612bf14
Jiraties <jiratiesstudios@gmail.com>
2021-09-30 21:23:02 +0700

    Scroll Blur and Lists
    yayyyyyy finaly done!

--- Software/Projects/Made by SS/Dev_News/asset/script/main.js
+++ Software/Projects/Made by SS/Dev_News/asset/script/main.js
@@ -34,11 +34,21 @@ setInterval(() => {
   document.getElementById("date").innerHTML = `${date} (Last updated 24 Sep)`;
 }, 1000);
 
+const hero = document.querySelector(".hero");
+let blurValue = 0;
+
 document.getElementById("body").onscroll = function myFunction() {
   let scrolltotop = document.scrollingElement.scrollTop;
   let target = document.querySelector(".hero");
   let xvalue = "center";
   let factor = 0.5;
   let yvalue = scrolltotop * factor;
+  console.log(scrolltotop / 1000)
+  scrolltotop / 1000 <= 3 ? blurValue = scrolltotop / 1000 : blurValue = 3;
+  console.log("Blur :" + blurValue)
+  // console.log("Hello")
   target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+  const hello = `blur(${blurValue}rem)`;
+  hero.style.filter = hello;
+
 };
