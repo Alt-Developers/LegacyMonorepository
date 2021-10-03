44c2e7eb14d8e4fc167e9e920255b4327cf7e3ae
SEN-SENSATION <prawich.th@gmail.com>
2021-10-04 01:18:29 +0700

    Portal 2.1 First Step
    Co-Authored-By: Saikaaa_ <87472782+Jiraties@users.noreply.github.com>

--- Software/Projects/Portal2.0/asset/script/main.js
+++ Software/Projects/Portal2.0/asset/script/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const text = [" Our website", " Our portal", " Our project", " Our World"];
+const text = [" Our Website", " Our portal", " Our project", " Our World"];
 let count = 0;
 let index = 0;
 let currentText = "";
@@ -8,19 +8,19 @@ let letter = "";
 let now;
 
 (function type() {
+  if (count === text.length) {
+    count = 0;
+  }
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
 
-    if (count === text.length) {
-        count = 0;
-    }
-    currentText = text[count];
-    letter = currentText.slice(0, ++index);
-
-    now = `Welcome to, ${letter}`;
-    document.getElementById("typing").innerHTML = now;
-    if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-    }
-    setTimeout(type, 400);
-
+  now = `Welcome to,${letter}`;
+  document.getElementById("typing").innerHTML = now;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
 })();
+
+const clickMe = function () {};
